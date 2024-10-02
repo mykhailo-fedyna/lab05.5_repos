16ed396cda26642aaecccbae786dc785ada62978
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-02 16:25:55 +0300

    first master commit

---
+++ Lab5.5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int geometricTerm(int b1, int q, int n, int& depth) {
+    depth++;
+    if (n == 1) {
+        return b1;
+    }
+    return q * geometricTerm(b1, q, n - 1, depth);
+}
+int geometricSum(int b1, int q, int n, int& depth) {
+    depth++;
+    if (n == 1) {
+        return b1;
+    }
+    return geometricTerm(b1, q, n, depth) + geometricSum(b1, q, n - 1, depth);
+}
+int main() {
+    int b1, q, n;
+    cout << "b1: ";
+    cin >> b1;
+    cout << "q: ";
+    cin >> q;
+    cout << "n: ";
+    cin >> n;
+    int depthTerm = 0;
+    int depthSum = 0;
+    int nthTerm = geometricTerm(b1, q, n, depthTerm);
+    cout << "The " << n << "-th term of the geometric progression: " << nthTerm << endl;
+    cout << "Recursion depth for finding the " << n << "-th term: " << depthTerm << endl;
+    int sum = geometricSum(b1, q, n, depthSum);
+    cout << "The sum of the first " << n << " terms of the progression: " << sum << endl;
+    cout << "Recursion depth for sum: " << depthSum << endl;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.5.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestGeometricTerm)
+		{
+			int depth = 0;
+			Assert::AreEqual(2, geometricTerm(2, 3, 1, depth));
+			Assert::AreEqual(6, geometricTerm(2, 3, 2, depth));
+		}
+		TEST_METHOD(TestGeometricSun)
+		{
+			int depth = 0;
+			Assert::AreEqual(2, geometricSum(2, 3, 1, depth));
+			Assert::AreEqual(8, geometricSum(2, 3, 2, depth));
+		}
+	};
+}
