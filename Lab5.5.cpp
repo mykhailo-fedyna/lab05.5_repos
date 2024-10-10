0d52ec47d9edb3592245b067ef220cbd69ef5777
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-10 14:52:06 +0300

    changed code

--- Lab5.5.cpp
+++ Lab5.5.cpp
@@ -1,35 +1,49 @@
-﻿#include <iostream>
-#include <cmath>
+﻿#include <cmath>
+#include <iostream>
 using namespace std;
-int geometricTerm(int b1, int q, int n, int& depth) {
-    depth++;
+
+int geometricTerm(int b1, int q, int n, int level, int& maxDepth) {
+    if (level > maxDepth) {
+        maxDepth = level;
+    }
+
     if (n == 1) {
         return b1;
     }
-    return q * geometricTerm(b1, q, n - 1, depth);
+
+    cout << "level = " << level << endl;
+    return q * geometricTerm(b1, q, n - 1, level + 1, maxDepth);
 }
-int geometricSum(int b1, int q, int n, int& depth) {
-    depth++;
+
+int geometricSum(int b1, int q, int n, int level, int& maxDepth) {
+    if (level > maxDepth) {
+        maxDepth = level;
+    }
+
     if (n == 1) {
         return b1;
     }
-    return geometricTerm(b1, q, n, depth) + geometricSum(b1, q, n - 1, depth);
+
+    cout << "level = " << level << endl;
+    return geometricTerm(b1, q, n, level + 1, maxDepth) + geometricSum(b1, q, n - 1, level + 1, maxDepth);
 }
+
 int main() {
     int b1, q, n;
-    cout << "b1: ";
-    cin >> b1;
-    cout << "q: ";
-    cin >> q;
-    cout << "n: ";
-    cin >> n;
-    int depthTerm = 0;
-    int depthSum = 0;
-    int nthTerm = geometricTerm(b1, q, n, depthTerm);
+    cout << "b1: "; cin >> b1;
+    cout << "q: "; cin >> q;
+    cout << "n: "; cin >> n;
+
+    int maxDepthTerm = 0;
+    int maxDepthSum = 0;
+
+    int nthTerm = geometricTerm(b1, q, n, 1, maxDepthTerm);
     cout << "The " << n << "-th term of the geometric progression: " << nthTerm << endl;
-    cout << "Recursion depth for finding the " << n << "-th term: " << depthTerm << endl;
-    int sum = geometricSum(b1, q, n, depthSum);
+    cout << "Maximum recursion depth for finding the " << n << "-th term: " << maxDepthTerm << endl;
+
+    int sum = geometricSum(b1, q, n, 1, maxDepthSum);
     cout << "The sum of the first " << n << " terms of the progression: " << sum << endl;
-    cout << "Recursion depth for sum: " << depthSum << endl;
+    cout << "Maximum recursion depth for sum: " << maxDepthSum << endl;
+
     return 0;
 }
--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 #include "../Lab5.5.cpp"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
 namespace UnitTest1
 {
 	TEST_CLASS(UnitTest1)
@@ -10,15 +11,16 @@ namespace UnitTest1
 
 		TEST_METHOD(TestGeometricTerm)
 		{
-			int depth = 0;
-			Assert::AreEqual(2, geometricTerm(2, 3, 1, depth));
-			Assert::AreEqual(6, geometricTerm(2, 3, 2, depth));
+			int maxDepth = 0;
+			Assert::AreEqual(2, geometricTerm(2, 3, 1, 1, maxDepth));
+			Assert::AreEqual(6, geometricTerm(2, 3, 2, 1, maxDepth));
 		}
-		TEST_METHOD(TestGeometricSun)
+
+		TEST_METHOD(TestGeometricSum)
 		{
-			int depth = 0;
-			Assert::AreEqual(2, geometricSum(2, 3, 1, depth));
-			Assert::AreEqual(8, geometricSum(2, 3, 2, depth));
+			int maxDepth = 0;
+			Assert::AreEqual(2, geometricSum(2, 3, 1, 1, maxDepth));
+			Assert::AreEqual(8, geometricSum(2, 3, 2, 1, maxDepth));
 		}
 	};
 }
